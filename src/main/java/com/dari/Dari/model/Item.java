e150def099a697e45665640b632255dfdf532875
ou5ama <lasri.oussama@gmail.com>
2023-08-12 16:25:29 +0100

    setting up model package, and building Item class that has (id, name, address, price, favorite, state, pictures, and houseParts)

---
+++ src/main/java/com/dari/Dari/model/Item.java
@@ -0,0 +1,86 @@
+package com.dari.Dari.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private Double price;
+    private Boolean favorite;
+    @Enumerated(EnumType.STRING)
+    private ItemState state;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    private List<Picture> pictures;
+    @ManyToMany
+    private List<HousePart> houseParts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+    public List<HousePart> getHouseParts() {
+        return houseParts;
+    }
+
+    public void setHouseParts(List<HousePart> houseParts) {
+        this.houseParts = houseParts;
+    }
+}
