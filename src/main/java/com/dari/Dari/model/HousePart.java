13930ce7c1fce9be75e0c8a71ed783a6f39eac9b
ou5ama <lasri.oussama@gmail.com>
2023-08-18 16:02:44 +0100

    Modify the house part Entity

--- src/main/java/com/dari/Dari/model/HousePart.java
+++ src/main/java/com/dari/Dari/model/HousePart.java
@@ -2,18 +2,31 @@ package com.dari.Dari.model;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
 public class HousePart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    @ManyToMany(mappedBy = "houseParts")
-    private List<Item> items;
 
-//  Getters and Setters
+    @Enumerated(EnumType.STRING)
+    private HousePartEnum part;
+
+    private int value;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+//    Constructors, Getter, Setters
+    public HousePart() {
+    }
+
+    public HousePart(Long id, HousePartEnum part, int value, Item item) {
+        this.id = id;
+        this.part = part;
+        this.value = value;
+        this.item = item;
+    }
 
     public Long getId() {
         return id;
@@ -23,19 +36,27 @@ public class HousePart {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public HousePartEnum getPart() {
+        return part;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPart(HousePartEnum part) {
+        this.part = part;
     }
 
-    public List<Item> getItems() {
-        return items;
+    public int getValue() {
+        return value;
     }
 
-    public void setItems(List<Item> items) {
-        this.items = items;
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
     }
 }
