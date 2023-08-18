ad64a503fa23d274073fc812a660bd7f0b172209
ou5ama <lasri.oussama@gmail.com>
2023-08-18 16:22:03 +0100

    create a User Entity

--- src/main/java/com/dari/Dari/model/User.java
+++ src/main/java/com/dari/Dari/model/User.java
@@ -1,4 +1,113 @@
 package com.dari.Dari.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private boolean active;
+
+    @OneToMany(mappedBy = "user")
+    private List<Item> favoriteItems;
+
+//    Constructors, getters, setters, and other methods
+
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email, String password, String firstName, String lastName, String phoneNumber, boolean active, List<Item> favoriteItems) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.active = active;
+        this.favoriteItems = favoriteItems;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<Item> getFavoriteItems() {
+        return favoriteItems;
+    }
+
+    public void setFavoriteItems(List<Item> favoriteItems) {
+        this.favoriteItems = favoriteItems;
+    }
 }
