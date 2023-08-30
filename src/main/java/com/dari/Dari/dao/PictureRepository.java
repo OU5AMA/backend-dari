b6d5174c1090b29a3844b46c107f2ff165aaff90
ou5ama <lasri.oussama@gmail.com>
2023-08-30 16:15:55 +0100

    modify picture and user Repository, use annotation

--- src/main/java/com/dari/Dari/dao/PictureRepository.java
+++ src/main/java/com/dari/Dari/dao/PictureRepository.java
@@ -2,6 +2,8 @@ package com.dari.Dari.dao;
 
 import com.dari.Dari.model.Pictures;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PictureRepository extends JpaRepository<Pictures, Long> {
 }
--- src/main/java/com/dari/Dari/dao/UserRepository.java
+++ src/main/java/com/dari/Dari/dao/UserRepository.java
@@ -2,6 +2,8 @@ package com.dari.Dari.dao;
 
 import com.dari.Dari.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
