341571a7c77d63cda5cdbbfff71e8a3cfffb013b
ou5ama <lasri.oussama@gmail.com>
2023-08-30 16:14:46 +0100

    modify Item Repository, use annotation

--- src/main/java/com/dari/Dari/dao/ItemRepository.java
+++ src/main/java/com/dari/Dari/dao/ItemRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.dari.Dari.model.User;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 //    find items by location(city)
