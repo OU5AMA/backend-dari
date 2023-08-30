b7eb23d21dd3841e0bc5c439248106330d330d9b
ou5ama <lasri.oussama@gmail.com>
2023-08-30 16:02:56 +0100

    implement lombok and modify a constructor

--- src/main/java/com/dari/Dari/model/HousePart.java
+++ src/main/java/com/dari/Dari/model/HousePart.java
@@ -2,62 +2,27 @@ package com.dari.Dari.model;
 
 import com.dari.Dari.model.enums.HousePartEnum;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class HousePart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long houseId;
 
     @Enumerated(EnumType.STRING)
     private HousePartEnum part;
-
     private int value;
-
     @ManyToOne
     @JoinColumn(name = "item_id")
     private Item item;
 
-//    Constructors, Getter, Setters
-    public HousePart() {
-    }
-
-    public HousePart(Long id, HousePartEnum part, int value, Item item) {
-        this.id = id;
+    public HousePart(HousePartEnum part, int value, Item item) {
         this.part = part;
         this.value = value;
-        this.item = item;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public HousePartEnum getPart() {
-        return part;
-    }
-
-    public void setPart(HousePartEnum part) {
-        this.part = part;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
         this.item = item;
     }
 }
